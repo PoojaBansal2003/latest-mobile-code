a1da3527e81eb310a85399a855aa12691e94a179
MukulDhy <1029mukul38@gmail.com>
2025-05-06 17:36:21 +0530

    more changes

--- App.js
+++ App.js
@@ -1,40 +1,33 @@
 import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as ReduxProvider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import WelcomeScreen from "./src/screens/WelcomeScreen";
-import UserTypeScreen from "./src/screens/UserTypeScreen";
-import CaregiverLoginScreen from "./src/screens/CaregiverLoginScreen";
-import CaregiverSignupScreen from "./src/screens/CaregivenSignupScreen";
-import PatientHome from "./src/screens/PatientHome";
-import HomeScreen from "./src/screens/HomeScreen";
-import SensorScreen from "./src/screens/SensorScreen";
-import FriendsFamilyMemoriesScreen from "./src/screens/FriendsFamilyMemoriesScreen";
-import PatientLoginScreen from "./src/screens/PatientLoginScreen";
-import VoiceRecordingScreen from "./src/screens/VoiceRecordingScreen";
-import ReminderScreen from "./src/screens/ReminderScreen";
-import FamilyMemberScreen from "./src/screens/AddFmDeviceScreen";
-import FamilyMembersPage from "./src/screens/FamilyMembersPage";
+import { store } from "./store";
+import AppNavigator from "./navigation/AppNavigator";
+import { useTheme } from "./themes/ThemeContext";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 
-const Stack = createStackNavigator();
-
-export default function App() {
+const AppWrapper = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="UserTypeScreen" component={UserTypeScreen} /> 
-        <Stack.Screen name="PatientHome" component={PatientHome} />
-        <Stack.Screen name="CaregiverLoginScreen" component={CaregiverLoginScreen}/>
-        <Stack.Screen name="CaregiverSignupScreen" component={CaregiverSignupScreen}/>
-        <Stack.Screen name="ReminderScreen" component={ReminderScreen}/>
-        <Stack.Screen name="AddFamily&DeviceScreen" component={FamilyMemberScreen}/>
-        <Stack.Screen name="FamilyMembersPage" component={FamilyMembersPage}/>
-        <Stack.Screen name="SensorDataScreen" component={SensorScreen}/>
-        <Stack.Screen name="FriendsFamilyMemoriesScreen" component={FriendsFamilyMemoriesScreen} />
-        <Stack.Screen name="PatientLoginScreen" component={PatientLoginScreen} />
-        <Stack.Screen name="VoiceRecordingScreen" component={VoiceRecordingScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </ReduxProvider>
   );
-}
+};
+
+const App = () => {
+  const { theme } = useTheme();
+
+  return (
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </PaperProvider>
+  );
+};
+
+export default AppWrapper;
---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,127 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
+import { useTranslation } from "react-i18next";
+import { ThemeColors } from "../themes/theme";
+
+// Screens for main app (tab navigator)
+import HomeScreen from "../screens/HomeScreen";
+import AddReminderScreen from "../screens/AddReminderScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+// Screens for auth stack
+import WelcomeScreen from "../screens/WelcomeScreen";
+import UserTypeScreen from "../screens/UserTypeScreen";
+import CaregiverLoginScreen from "../screens/CaregiverLoginScreen";
+import CaregiverSignupScreen from "../screens/CaregiverSignupScreen";
+import PatientHome from "../screens/PatientHome";
+import SensorScreen from "../screens/SensorScreen";
+import FriendsFamilyMemoriesScreen from "../screens/FriendsFamilyMemoriesScreen";
+import PatientLoginScreen from "../screens/PatientLoginScreen";
+import VoiceRecordingScreen from "../screens/VoiceRecordingScreen";
+import ReminderScreen from "../screens/ReminderScreen";
+import FamilyMemberScreen from "../screens/AddFmDeviceScreen";
+import FamilyMembersPage from "../screens/FamilyMembersPage";
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+
+const HomeStackNavigator = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+    </HomeStack.Navigator>
+  );
+};
+
+const AppTabNavigator = () => {
+  const { t } = useTranslation();
+  const theme = ThemeColors();
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "AddReminder") {
+            iconName = focused ? "add-circle" : "add-circle-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: "gray",
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeStackNavigator}
+        options={{ title: t("home") }}
+      />
+      <Tab.Screen
+        name="AddReminder"
+        component={AddReminderScreen}
+        options={{ title: t("addReminder") }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{ title: t("settings") }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* Auth Screens */}
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="UserTypeScreen" component={UserTypeScreen} />
+        <Stack.Screen
+          name="CaregiverLoginScreen"
+          component={CaregiverLoginScreen}
+        />
+        <Stack.Screen
+          name="CaregiverSignupScreen"
+          component={CaregiverSignupScreen}
+        />
+        <Stack.Screen
+          name="PatientLoginScreen"
+          component={PatientLoginScreen}
+        />
+
+        {/* Main App Screens */}
+        <Stack.Screen name="MainApp" component={AppTabNavigator} />
+        <Stack.Screen name="PatientHome" component={PatientHome} />
+        <Stack.Screen name="ReminderScreen" component={ReminderScreen} />
+        <Stack.Screen
+          name="AddFamily&DeviceScreen"
+          component={FamilyMemberScreen}
+        />
+        <Stack.Screen name="FamilyMembersPage" component={FamilyMembersPage} />
+        <Stack.Screen name="SensorDataScreen" component={SensorScreen} />
+        <Stack.Screen
+          name="FriendsFamilyMemoriesScreen"
+          component={FriendsFamilyMemoriesScreen}
+        />
+        <Stack.Screen
+          name="VoiceRecordingScreen"
+          component={VoiceRecordingScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;

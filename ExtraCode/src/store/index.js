ed1b2cb5cb1804187df11c5a14ad346c20531c79
Sumit-techy <sumitmaan1606@gmail.com>
2025-05-06 18:15:41 +0530

    chnages more made

--- ExtraCode/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import reminderReducer from '../features/reminders/reminderSlice';
-
-export const store = configureStore({
-  reducer: {
-    reminders: reminderReducer,
-  },
-});
